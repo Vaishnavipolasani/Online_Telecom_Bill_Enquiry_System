9b78864e57f5fb3f28626252bed3fc157c9e092f
priyanka <priyankapmanture@gmail.com>
2024-11-22 18:19:14 +0530

    Done by team3

--- src/main/java/com/otbs/model/Complaint.java
+++ src/main/java/com/otbs/model/Complaint.java
@@ -1,6 +1,13 @@
 package com.otbs.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Complaint {
+	@Id
 	
 	private int complaintId;
 	private User userObj;
@@ -12,6 +19,11 @@ public class Complaint {
     private String complaintType;
     private String status;
     private String priority;
-
+@ManyToOne
+@JoinColumn(name="raised_by",nullable=false)
+private Customer raisedBy;
+@ManyToOne
+@JoinColumn(name="assigned_to",nullable=false)
+private HelpDeskExecutive assignedTo;
 
 }
